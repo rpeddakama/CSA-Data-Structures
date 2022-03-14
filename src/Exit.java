663227991252e9b52b414a10fa59f52f45984285
rpeddakama <rishipeddakama@gmail.com>
2022-03-13 22:41:04 -0700

    add exit program option

---
+++ src/Exit.java
@@ -0,0 +1,12 @@
+package src;
+
+public class Exit extends Blueprint {
+    public Exit(String optionTitle) {
+        super(optionTitle);
+    }
+
+    public void run() {
+        System.exit(0);
+    }
+
+}
