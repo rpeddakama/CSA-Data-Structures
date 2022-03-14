9415f9b9991e394ede8c51c9803e6de33452dc3f
rpeddakama <rishipeddakama@gmail.com>
2022-03-13 22:40:50 -0700

    create sample option class

---
+++ src/Sample.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Sample extends Blueprint {
+    public Sample(String optionTitle) {
+        super(optionTitle);
+    }
+
+    public void run() {
+        System.out.println("This is the sample yo");
+    }
+}
