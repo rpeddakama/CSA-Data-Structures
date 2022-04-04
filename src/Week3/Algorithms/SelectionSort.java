6012a6ce902c49b7c076c881f1a5c948c5364589
rpeddakama <rishipeddakama@gmail.com>
2022-04-03 22:39:03 -0700

    create files

---
+++ src/Week3/Algorithms/SelectionSort.java
@@ -0,0 +1,5 @@
+package src.Week3.Algorithms;
+
+public class SelectionSort {
+
+}
---
+++ src/Week3/Sorts.java
@@ -0,0 +1,13 @@
+package src.Week3;
+
+import src.Blueprint;
+
+public class Sorts extends Blueprint {
+    public Sorts(String optionTitle) {
+        super(optionTitle);
+    }
+
+    public void run() {
+
+    }
+}
